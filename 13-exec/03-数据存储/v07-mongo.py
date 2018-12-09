951db1073d7f92a7957209fb147af4a28e4a3687
xrr314 <xrr940314>
2018-12-09 20:19:05 +0800

    forAllPython

--- 13-exec/03-数据存储/v07-mongo.py
+++ 13-exec/03-数据存储/v07-mongo.py
@@ -12,4 +12,47 @@ row	        document	行/文档
 column	    field	    字段(列)/域
 index	    index	    索引
 primary     primary	    主键/MongoDB自动将_id字段设置为主键
+
+mongodb安装(我们这里已经安装了)
+    1.https://jingyan.baidu.com/article/e5c39bf5f5ddd539d76033a9.html
+    2.sudo apt-get install mongodb
+
 '''
+
+import pymongo
+
+# # 查询
+# # 连接数据库(3种方式)
+# # 默认连接本地
+# # client = pymongo.MongoClient()
+# client = pymongo.MongoClient('127.0.0.1',27017)
+# # client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+#
+# # 获取数据库
+# db = client.Dengzi
+# # 获取集合
+# std = db.students
+#
+# # 获取数据
+# datas = std.find()
+# print(datas,type(datas))
+# for data in datas:
+#     print(data['name'],data['age'])
+
+# 插入文档(行)
+client = pymongo.MongoClient()
+
+db = client.test
+std = db.person
+post = {
+    'name':'xue',
+    'age':18,
+    'sex':'男',
+    'class':['database','python','java','math'],
+    'income':5555
+}
+
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+print('post_id:%s'%(post_id))
+
